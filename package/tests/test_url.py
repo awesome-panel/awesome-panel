0ea3bef3f90c9a8379a7e52267d2f4c4c097cbe5
Marc Skov Madsen <MASMA@orsted.dk>
2019-11-26 18:53:42 +0100

    moved test

--- package/tests/test_url.py
+++ package/tests/test_url.py
@@ -31,15 +31,17 @@ def test_pn_url():
     """Manual Test"""
     # Given
     country_url = _Country()
-    panel = pn.Column(country_url.param, country_url.set_browser_url_parameters)
+    text = """
+Check
+
+- opening [http://localhost:5006/test_url](http://localhost:5006/test_url) works without error
+- opening [http://localhost:5006/test_url?country=](http://localhost:5006/test_url?country=) works without error
+- opening [http://localhost:5006/test_url?country=Denmark](http://localhost:5006/test_url?country=Denmark) then the country widget parameter is set to Denmark
+- Changing the country widget parameter to Norway changes the browser url to
+[http://localhost:5006/test_url?country=Norway](http://localhost:5006/test_url?country=Norway)
+    """
+    panel = pn.Column(country_url.param, text, country_url.set_browser_url_parameters)
     panel.servable()
-    # When
-    # 0. opening http://localhost:5006/test_url works without error
-    # 2. opening http://localhost:5006/test_url?country= works without error
-    # 3. opening http://localhost:5006/test_url?country=Denmark
-    # then the country widget parameter is set to Denmark
-    # 4. Changing the country widget parameter to Norway changes the browser url to
-    # http://localhost:5006/test_url?country=Norway
 
 
 if __name__.startswith("bk_script"):
