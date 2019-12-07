16b99b4411c7fbae7042e6eb50d5d57a00c9697c
Marc Skov Madsen <MASMA@orsted.dk>
2019-12-07 08:00:15 +0100

    changed to small app

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from gallery.bootstrap_dashboard.main import main as bootstrap_dashboard_app
+# from gallery.bootstrap_dashboard.main import main as bootstrap_dashboard_app
+# bootstrap_dashboard_app().servable()
 
-bootstrap_dashboard_app().servable()
+import panel as pn
+
+pn.pane.Markdown("# Awesome Panel").servable()
 
