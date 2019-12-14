6bc9035644a1745567b289f9a767a0b9b98760b3
Marc Skov Madsen <MASMA@orsted.dk>
2019-12-14 07:54:08 +0100

    added test_fontawesome to gallery

--- package/awesome_panel/database/apps_in_gallery.py
+++ package/awesome_panel/database/apps_in_gallery.py
@@ -7,8 +7,7 @@ gallery/<app_name_folder>
 - include that tags tags.CODE, tags.APP_IN_GALLERY as a minimum
 """
 from awesome_panel.database import authors, tags
-from awesome_panel.database.settings import (GITHUB_BLOB_MASTER_URL,
-                                             THUMBNAILS_ROOT)
+from awesome_panel.database.settings import GITHUB_BLOB_MASTER_URL, THUMBNAILS_ROOT
 from awesome_panel.shared.models import Resource
 
 GITHUB_GALLERY_URL = GITHUB_BLOB_MASTER_URL + "gallery/"
@@ -26,6 +25,14 @@ APPS_IN_GALLERY = [
         name="Test Spinners",
         url=GITHUB_GALLERY_URL + "awesome_panel_express_tests/test_spinners.py",
         thumbnail_path=THUMBNAILS_ROOT + "test_spinners.png",
+        tags=[tags.CODE, tags.APP_IN_GALLERY, tags.AWESOMEPANEL_EXPRESS],
+        is_awesome=True,
+        author=authors.MARC_SKOV_MADSEN,
+    ),
+    Resource(
+        name="Test FontAwesome",
+        url=GITHUB_GALLERY_URL + "awesome_panel_express_tests/test_fontawesome.py",
+        thumbnail_path=THUMBNAILS_ROOT + "test_fontawesome.png",
         tags=[tags.CODE, tags.APP_IN_GALLERY, tags.AWESOMEPANEL_EXPRESS],
         is_awesome=True,
         author=authors.MARC_SKOV_MADSEN,
