aabace1793d50facfcc953a3c3cee004f2a5cf04
marc.skov.madsen@gmail.com <marc.skov.madsen@gmail.com>
2020-01-12 06:38:14 +0100

    all tests pass

--- tests/src/page/gallery/test_yahooquery_app.py
+++ tests/src/page/gallery/test_yahooquery_app.py
@@ -1,5 +1,6 @@
 """In this module we test the YahooQuery App"""
-from src.pages.gallery.yahooquery_app.yahooquery_app import BasePage, Ticker, YahooQueryService
+# pylint: disable=redefined-outer-name,protected-access
+from src.pages.gallery.yahooquery_app.yahooquery_app import BasePage  # type: ignore
 
 
 def test_base__page__data_balance_sheet_quarterly():
@@ -7,10 +8,10 @@ def test_base__page__data_balance_sheet_quarterly():
     base_page = BasePage()
     base_page.symbols = "ORSTED.CO"
     base_page.endpoint = "balance_sheet"
-    base_page.frequency = "quarterly"
+    base_page.frequency = "q"
 
     # assert isinstance(base_page.attr, Ticker.balance_sheet)
-    assert base_page.attr_is_property == False
+    assert not base_page.attr_is_property
 
     base_page._help()
     base_page.frequency_edit_view()
