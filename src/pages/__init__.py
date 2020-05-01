5ca98c972fd76138dcebf0b0f46012e2f321c8c9
marc.skov.madsen@gmail.com <marc.skov.madsen@gmail.com>
2020-05-01 05:38:11 +0200

    added the remainding gallery pages

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -1,6 +1,21 @@
+from src.pages.awesome_panel_express_tests import (
+    test_code,
+    test_bootstrap_alerts,
+    test_bootstrap_card,
+    test_dataframe,
+    test_divider,
+    test_fontawesome,
+    test_headings,
+    test_markdown,
+    test_progress_ext,
+    test_share_links,
+    test_spinners,
+)
 from src.pages.bootstrap_dashboard import bootstrap_dashboard
 from src.pages.custom_bokeh_model import custom_bokeh_model
 from src.pages.dataexplorer_loading.dataexplorer_loading import DataExplorer
 from src.pages.image_classifier import image_classifier
 from src.pages.kickstarter_dashboard import kickstarter_dashboard
 from src.pages.owid_choropleth_map import owid_choropleth_map
+from src.pages.param_reference_example import param_reference_example
+from src.pages.yahooquery_app import yahooquery_app
