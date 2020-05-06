6e93e3d371d07231bc4cc4e26f228900773d6a29
marc.skov.madsen@gmail.com <marc.skov.madsen@gmail.com>
2020-05-06 06:06:48 +0200

    added ApplicationView

--- package/awesome_panel/application/views/application_view.py
+++ package/awesome_panel/application/views/application_view.py
@@ -1,4 +1,87 @@
 import param
+import panel as pn
 
-class ApplicationView(param.Parameterized):
-    pass
+
+class ApplicationView(pn.Template):
+    main = param.ClassSelector(class_=pn.layout.Reactive, allow_None=False, constant=True)
+    sidebar = param.ClassSelector(class_=pn.layout.Reactive, allow_None=False, constant=True)
+    topbar = param.ClassSelector(class_=pn.layout.Reactive, allow_None=False, constant=True)
+    spinner = param.ClassSelector(class_=pn.layout.Reactive, allow_None=False, constant=True)
+    navigation = param.ClassSelector(class_=pn.layout.Reactive, allow_None=False, constant=True)
+
+    application_css = param.ClassSelector(class_=pn.pane.HTML, allow_None=False, constant=True)
+    application_js = param.ClassSelector(class_=pn.pane.HTML, allow_None=False, constant=True)
+
+    theme_css = param.ClassSelector(class_=pn.pane.HTML, allow_None=False, constant=True)
+    theme_js = param.ClassSelector(class_=pn.pane.HTML, allow_None=False, constant=True)
+
+    page_css = param.ClassSelector(class_=pn.pane.HTML, allow_None=False, constant=True)
+    page_js = param.ClassSelector(class_=pn.pane.HTML, allow_None=False, constant=True)
+
+    def __init__(self, **params):
+        if "main" not in params:
+            params["main"] = self._get_main()
+        if "sidebar" not in params:
+            params["sidebar"] = self._get_sidebar()
+        if "topbar" not in params:
+            params["topbar"] = self._get_topbar()
+        if "spinner" not in params:
+            params["spinner"] = self._get_spinner()
+        if "navigation" not in params:
+            params["navigation"] = self._get_navigation()
+        if "application_css" not in params:
+            params["application_css"] = self._get_invisible_html_pane("aplication_css")
+        if "application_js" not in params:
+            params["application_js"] = self._get_invisible_html_pane("aplication_js")
+        if "theme_css" not in params:
+            params["theme_css"] = self._get_invisible_html_pane("theme_css")
+        if "theme_js" not in params:
+            params["theme_js"] = self._get_invisible_html_pane("theme_js")
+        if "page_css" not in params:
+            params["page_css"] = self._get_invisible_html_pane("page_css")
+        if "page_js" not in params:
+            params["page_js"] = self._get_invisible_html_pane("page_js")
+
+        super().__init__(**params)
+
+        self.add_panel(name="main", panel=self.main)
+        self.add_panel(name="sidebar", panel=self.sidebar)
+        self.add_panel(name="topbar", panel=self.topbar)
+        self.add_panel(name="spinner", panel=self.spinner)
+        self.add_panel(name="navigation", panel=self.navigation)
+        self.add_panel(name="application_css", panel=self.application_css)
+        self.add_panel(name="application_js", panel=self.application_js)
+        self.add_panel(name="theme_css", panel=self.theme_css)
+        self.add_panel(name="theme_js", panel=self.theme_js)
+        self.add_panel(name="page_css", panel=self.page_css)
+        self.add_panel(name="page_js", panel=self.page_js)
+
+    @staticmethod
+    def _get_main():
+        return pn.Column(
+            name="ApplicationComponent.main",
+            css_classes=["main"],
+            sizing_mode="stretch_both",
+            margin=(25, 50, 50, 50),
+        )
+
+    @staticmethod
+    def _get_sidebar():
+        return pn.Column(name="ApplicationComponent.sidebar")
+
+    @staticmethod
+    def _get_topbar():
+        return pn.Row(name="ApplicationComponent.topbar", sizing_mode="stretch_width")
+
+    @staticmethod
+    def _get_navigation():
+        return pn.Row(name="ApplicationComponent.navigation", sizing_mode="stretch_width")
+
+    @staticmethod
+    def _get_spinner():
+        return pn.Column(name="ApplicationComponent.spinner", sizing_mode="stretch_both")
+
+    @staticmethod
+    def _get_invisible_html_pane(name):
+        name = f"ApplicationComponent.{name.lower()}"
+        return pn.pane.HTML(name=name, height=0, width=0, sizing_mode="fixed", margin=0)
---
+++ package/tests/application/views/test_application_view.py
@@ -0,0 +1,46 @@
+import panel as pn
+from awesome_panel.application.views import ApplicationView
+
+
+def test_can_construct(application_view):
+    assert isinstance(application_view, ApplicationView)
+    assert hasattr(application_view, "main")
+    assert hasattr(application_view, "sidebar")
+    assert hasattr(application_view, "topbar")
+    assert hasattr(application_view, "spinner")
+    assert hasattr(application_view, "navigation")
+
+    assert isinstance(application_view.application_css, pn.pane.HTML)
+    assert isinstance(application_view.application_js, pn.pane.HTML)
+    assert isinstance(application_view.theme_css, pn.pane.HTML)
+    assert isinstance(application_view.theme_js, pn.pane.HTML)
+    assert isinstance(application_view.page_css, pn.pane.HTML)
+    assert isinstance(application_view.page_js, pn.pane.HTML)
+
+    assert application_view.application_js.sizing_mode == "fixed"
+    assert application_view.application_css.sizing_mode == "fixed"
+    assert application_view.theme_css.sizing_mode == "fixed"
+    assert application_view.theme_js.sizing_mode == "fixed"
+    assert application_view.page_css.sizing_mode == "fixed"
+    assert application_view.page_js.sizing_mode == "fixed"
+
+    assert application_view.application_js.width == 0
+    assert application_view.application_css.width == 0
+    assert application_view.theme_css.width == 0
+    assert application_view.theme_js.width == 0
+    assert application_view.page_css.width == 0
+    assert application_view.page_js.width == 0
+
+    assert application_view.application_js.height == 0
+    assert application_view.application_css.height == 0
+    assert application_view.theme_css.height == 0
+    assert application_view.theme_js.height == 0
+    assert application_view.page_css.height == 0
+    assert application_view.page_js.height == 0
+
+    assert application_view.application_js.margin == 0
+    assert application_view.application_css.margin == 0
+    assert application_view.theme_css.margin == 0
+    assert application_view.theme_js.margin == 0
+    assert application_view.page_css.margin == 0
+    assert application_view.page_js.margin == 0
