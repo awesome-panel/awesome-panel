482b8f3ba658cd6fc5baa810431054eb0e2b3c8b
marc.skov.madsen@gmail.com <marc.skov.madsen@gmail.com>
2020-05-10 17:02:27 +0200

    added more examples

--- package/tests/designer/test_panel_designer_app_show.py
+++ package/tests/designer/test_panel_designer_app_show.py
@@ -5,8 +5,8 @@ import param
 import pytest
 
 from awesome_panel.designer import PanelDesignerApp, services, components
-
-
+from awesome_panel.express import Card
+from awesome_panel.express.assets import BOOTSTRAP_PANEL_EXPRESS_CSS
 FIXTURES = pathlib.Path(__file__).parent / "fixtures"
 COMPONENT_CSS = FIXTURES / "component.css"
 COMPONENT_JS = FIXTURES / "component.js"
@@ -24,11 +24,25 @@ CENTERED_COMPONENT = services.ReloadService(
     js_path=COMPONENT_JS,
     component_parameters={"component": components.TitleComponent()},
 )
+STOPPED_COMPONENT = services.ReloadService(
+    component=components.StoppedComponent,
+    css_path=COMPONENT_CSS,
+    js_path=COMPONENT_JS,
+)
+CARD_COMPONENT = services.ReloadService(
+    component=Card,
+    css_path=BOOTSTRAP_PANEL_EXPRESS_CSS,
+    js_path=COMPONENT_JS,
+    component_parameters={"header": "Test Card", "body": pn.pane.Markdown("Awesome Panel "*50), "collapsable": True},
+)
+
 
 RELOAD_SERVICES = [
     TITLE_COMPONENT,
     EMPTY_COMPONENT,
     CENTERED_COMPONENT,
+    STOPPED_COMPONENT,
+    CARD_COMPONENT,
 ]
 
 
