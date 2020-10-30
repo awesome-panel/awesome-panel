9eefa5b53caf6377f4739222b61c1e28d94aa57b
Marc Skov Madsen <MASMA@orsted.dk>
2020-10-30 08:38:26 +0100

    fix mypy error

--- application/template/__init__.py
+++ application/template/__init__.py
@@ -1,2 +1,2 @@
 """You can use the get_template function to get an awesome-panel.org template"""
-from .template import get_template
+from .template import get_template, TEMPLATE_JS_ID, TEMPLATES, _get_template_js
