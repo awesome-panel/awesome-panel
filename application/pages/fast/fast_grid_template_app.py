0e8b119d33e6ccfe4cddeb992f0f994b0592c4be
Marc Skov Madsen <marc.skov.madsen@gmail.com>
2020-11-20 06:15:29 +0100

    added fast grid template example

--- application/pages/fast/fast_grid_template_app.py
+++ application/pages/fast/fast_grid_template_app.py
@@ -1,7 +1,23 @@
-"""The FastGridTemplate App demonstrates the use of the FastGridTemplate and the Fast components.
+# pylint: disable=line-too-long
+"""This app is based on the *`FastGridTemplate`* and the *Fast Components* provided by the
+<fast-anchor href="https://awesome-panel.readthedocs.io/en/latest/packages/awesome-panel-extensions/index.html#fast"
+appearance="hypertext" target="_blank">awesome-panel-extensions</fast-anchor>
+package.
 
-The app is layed out in a nice responsive layout provided by the FastGridTemplate.
+You can install the package via `pip install awesome-panel-extensions` and use the fast template
+and components via `from awesome_panel_extensions.frameworks import fast`.
+
+We are also using some Panel Components with Fast like CSS styling.
+
+## <a href="https://fast.design" target="_blank"><img src="https://explore.fast.design/e1e15bd85334e4346744078af2f52308.svg" style="vertical-align: middle; height: 32px;"></a>
+
+Fast is the adaptive interface system for modern web experiences.
+
+Interfaces built with FAST adapt to your design system and can be used with any modern UI Framework by leveraging industry standard Web Components.
+
+Checkout the <fast-anchor href="https://explore.fast.design/components/fast-accordion" appearance="hypertext" target="_blank">Component Gallery</fast-anchor>.
 """
+# pylint: enable=line-too-long
 import holoviews as hv
 import numpy as np
 import panel as pn
@@ -18,6 +34,7 @@ from awesome_panel_extensions.frameworks.fast.templates.fast_grid_template impor
 from holoviews import opts
 
 from application.config import site
+from application.pages.fast.echarts_app import EchartsApp
 
 hv.extension("bokeh")
 ellipse_opts = opts.Ellipse(line_width=3, color="#DF3874")
@@ -28,7 +45,7 @@ APPLICATION = site.create_application(
     author="Marc Skov Madsen",
     introduction="Demonstrates the FastGridTemplate and Fast Components",
     description=__doc__,
-    thumbnail_url="fast_grid_template.png",
+    thumbnail_url="fast-grid-template.png",
     documentation_url="",
     code_url="fast/fast_grid_template.py",
     gif_url="",
@@ -49,6 +66,7 @@ def _config_fast_size_mode():
 COLLAPSED_ICON = svg_icons.FAST_COLLAPSED_ICON
 EXPANDED_ICON = svg_icons.FAST_EXPANDED_ICON
 
+# pylint: disable=line-too-long
 NAVIGATION_HTML = f"""
 <fast-accordion>
 <fast-accordion-item slot="item" expanded>
@@ -62,7 +80,7 @@ NAVIGATION_HTML = f"""
         <h4>Main</h4>
     </div>{ COLLAPSED_ICON }{ EXPANDED_ICON }
 </fast-accordion-item>
-<fast-accordion-item slot="item">
+<fast-accordion-item slot="item" expanded>
     <div slot="heading">
         <h3>Share on Social</h3>
     </div>
@@ -78,25 +96,7 @@ NAVIGATION_HTML = f"""
 </fast-accordion-item>
 </fast-accordion>
 """
-
-INFO = """
-## <a href="https://fast.design" target="_blank"><img src="https://explore.fast.design/e1e15bd85334e4346744078af2f52308.svg" style="vertical-align: middle; height: 32px;"></a>
-
-The adaptive interface system for modern web experiences.
-
-Interfaces built with FAST adapt to your design system and can be used with any modern UI Framework by leveraging industry standard Web Components.
-
-Checkout the <fast-anchor href="https://explore.fast.design/components/fast-accordion" appearance="hypertext" target="_blank">Component Gallery</fast-anchor>.
-
-### Panel and Fast
-
-You can now use Fast with the HoloViz Panel framework. This app is based on the *`FastGridTemplate`* and the *Fast Components* provided by the
-<fast-anchor href="https://awesome-panel.readthedocs.io/en/latest/packages/awesome-panel-extensions/index.html#fast"
-appearance="hypertext" target="_blank">awesome-panel-extensions</fast-anchor>
-package. You can use it via `pip install awesome-panel-extensions` and
-`from awesome_panel_extensions.frameworks import fast`.
-"""
-
+# pylint: enable=line-too-long
 
 def _create_hvplot():
     # Generate some data
@@ -115,7 +115,12 @@ def _create_hvplot():
         * hv.Ellipse(2, 2, 2).opts(ellipse_opts)
         * hv.Ellipse(-2, -2, (4, 2)).opts(ellipse_opts)
     )
-    return plot
+    return pn.Column(
+        pn.pane.Markdown("## HoloViews Plot"),
+        pn.pane.HoloViews(plot, sizing_mode="stretch_both"),
+        sizing_mode="stretch_both",
+    )
+
 
 
 def _create_navigation_menu():
@@ -133,43 +138,102 @@ def _create_sidebar_items():
 
 
 def _create_fast_button_card():
-    button = FastButton(name="Click me", appearance="accent")
-    button.param.name.precedence = 0
-    button.param.clicks.precedence = 0
-    button.param.appearance.precedence = 0
-    button.param.disabled.precedence = 0
-    button.param.button_type.precedence = 0
-    button_parameters = [
-        "name",
-        "button_type",
+    component = FastButton(name="Click me", appearance="accent")
+    parameters = [
         "clicks",
-        "disabled",
+        "name",
         "appearance",
-        "width",
-        "height",
-        "sizing_mode",
+        "button_type",
     ]
     widgets = {
-        "name": FastTextInput,
         "clicks": {"disabled": True},
-        "disabled": {"type": FastCheckbox},
     }
+    return _create_card(component=component, parameters=parameters, widgets=widgets)
+
+
+def _create_fast_checkbox_card():
+    component = FastCheckbox(name="Check me", value=False)
+    parameters = [
+        "value",
+        "name",
+        "readonly",
+    ]
+    widgets = {
+        "value": FastCheckbox,
+        "readonly": FastCheckbox,
+    }
+    return _create_card(component=component, parameters=parameters, widgets=widgets)
+
+
+def _create_fast_literal_input_card():
+    component = FastLiteralInput(
+        name="Literal Input",
+        type=(type, dict),
+        value={"a": 1, "b": "Hello Fast"},
+        placeholder="Enter a dict",
+    )
+    parameters = [
+        "value",
+        "name",
+        # "type",
+        "placeholder",
+        "appearance",
+        "serializer",
+        "readonly",
+    ]
+    widgets = {
+        "value": FastLiteralInput,
+        "type": {"type": FastLiteralInput, "disabled": True},
+        "placeholder": FastTextInput,
+        "readonly": FastCheckbox,
+    }
+    return _create_card(component=component, parameters=parameters, widgets=widgets)
+
+def _create_fast_switch_card():
+    component = FastSwitch(
+        name="Notify by Email",
+        value=False,
+        checked_message="On",
+        unchecked_message="Off",
+    )
+    parameters=[
+            "value",
+            "name",
+            "checked_message",
+            "unchecked_message",
+            "readonly",
+        ]
+    widgets = {
+        "value": FastCheckbox,
+        "checked_message": FastTextInput,
+        "unchecked_message": FastTextInput,
+        "readonly": FastCheckbox
+    }
+    return _create_card(component=component, parameters=parameters, widgets=widgets)
+
+def _create_card(component, parameters, widgets):
+    parameters = [*parameters, "disabled", "width", "height", "sizing_mode"]
+    widgets["name"] = FastTextInput
+    widgets["disabled"] = FastCheckbox
+    for index, name in enumerate(parameters):
+        component.param[name].precedence = index
+    component.width=200
     settings = pn.Param(
-        button,
-        parameters=button_parameters,
+        component,
+        parameters=parameters,
         widgets=widgets,
         show_name=False,
         sizing_mode="stretch_width",
     )
     return pn.Column(
-        pn.pane.HTML("<h2>FastButton</h2>"),
-        button,
+        pn.pane.HTML(f"<h2>{component.__class__.name}</h2>"),
+        pn.Row(component, height=60),
         pn.pane.HTML("<h3>Parameters</h3>"),
         settings,
         sizing_mode="stretch_both",
     )
 
-
+@site.add(APPLICATION)
 def view():
     """Returns the FastGridTemplate App"""
     pn.config.sizing_mode = "stretch_width"
@@ -179,14 +243,17 @@ def view():
         title="FastGridTemplate by awesome-panel.org",
     )
 
-    app.main[0:6, 0:6] = pn.pane.Markdown(INFO, sizing_mode="stretch_both")
-    app.main[0:6, 6:12] = pn.pane.HoloViews(_create_hvplot(), sizing_mode="stretch_both")
-    app.main[6:18, 0:3] = _create_fast_button_card()
-    app.main[6:12, 6:12] = pn.pane.HoloViews(_create_hvplot(), sizing_mode="stretch_both")
-    app.main[18:36, 0:6] = APPLICATION.intro_section()
+    app.main[0:8, 0:6] = APPLICATION.intro_section()
+    app.main[0:8, 6:12] = _create_hvplot()
+    app.main[8:15, 0:12] = EchartsApp().view()
+    app.main[15:29, 0:3] = _create_fast_button_card()
+    app.main[15:29, 3:6] = _create_fast_checkbox_card()
+    app.main[15:29, 6:9] = _create_fast_literal_input_card()
+    app.main[15:29, 9:12] = _create_fast_switch_card()
     app.sidebar.extend(_create_sidebar_items())
     return app
 
 
 if __name__.startswith("bokeh"):
     view().servable()
+view()
