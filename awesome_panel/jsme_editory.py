9e9f655b862c8ba72ee4639e70ad15be30cb2692
Marc Skov Madsen <marc.skov.madsen@gmail.com>
2022-03-20 05:10:17 +0100

    add jsme editor example

---
+++ awesome_panel/jsme_editory.py
@@ -0,0 +1,24 @@
+import panel as pn 
+from panel_chemistry.widgets import JSMEEditor # panel_chemistry needs to be imported before you run pn.extension()
+
+from awesome_panel import config
+
+config.extension("jsme", url="jsme_editor")
+
+smiles="N[C@@H](CCC(=O)N[C@@H](CS)C(=O)NCC(=O)O)C(=O)O"
+editor = JSMEEditor(value=smiles, height=500, format="smiles").servable()
+
+values = pn.Param(editor, parameters=["value", "jme", "smiles", "mol", "mol3000", "sdf"], widgets={
+    "value": {"type": pn.widgets.TextAreaInput, "height": 200},
+    "jme": {"type": pn.widgets.TextAreaInput, "height": 200},
+    "smile": {"type": pn.widgets.TextAreaInput, "height": 200},
+    "mol": {"type": pn.widgets.TextAreaInput, "height": 200},
+    "mol3000": {"type": pn.widgets.TextAreaInput, "height": 200},
+    "sdf": {"type": pn.widgets.TextAreaInput, "height": 200},
+}).servable()
+
+settings = pn.Param(editor,
+    parameters=["height", "width", "sizing_mode", "subscriptions", "format", "options", "guicolor"],
+    widgets={
+        "options": {"height": 300},
+}).servable(area="sidebar")
